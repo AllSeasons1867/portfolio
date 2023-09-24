d5fc48cbe0f26b048327d2ba7e5cf49ef683606d
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-24 14:57:31 -0600

    added obfuscation for email address in js file

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var recipientEmail = 'wong.forrest@gmail.com';
+    var em1 = "wong.forrest"
+    var em2 = "@"
+    var em3 = "gmail.com"
+
+    var recipientEmail = em1 + em2 + em3;
     function openEmailClient() {
         var mailtoUrl = 'mailto:' + recipientEmail;
         window.location.href = mailtoUrl;
